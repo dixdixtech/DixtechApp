bb57a9b6aaec90bf7bf161af54b909b8805908fe
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-07 15:48:14 -0300

    Adicionando intents implícitas

--- app/src/main/java/com/example/dixtechapp/Sobre.java
+++ app/src/main/java/com/example/dixtechapp/Sobre.java
@@ -5,11 +5,52 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class Sobre extends AppCompatActivity {
+    
+    ImageButton botaoFace, botaoLinkedin, botaoInsta;
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sobre);
+        IniciarComponentes();
+        
+            
+        botaoFace.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.facebook.com/Dix-Tech-Corporation-116487190215728");
+                Intent it = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(Intent.createChooser(it, getString(R.string.chNav)));
+
+            }
+        });
+        
+        botaoLinkedin.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.linkedin.com/in/dix-tech-corporation-b83a961b8/");
+                Intent it = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(Intent.createChooser(it, getString(R.string.chNav)));
+
+            }
+        });
+        
+        botaoInsta.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Uri uri = Uri.parse("https://www.instagram.com/dix_tech/");
+                Intent it = new Intent(Intent.ACTION_VIEW,uri);
+                startActivity(Intent.createChooser(it, getString(R.string.chNav)));
+
+            }
+        });
+    }
+    
+    private void IniciarComponentes(){
+        botaoFace = findViewById(R.id.btnface);
+        botaoLinkedin = findViewById(R.id.btnlinkedin);
+        botaoInsta = findViewById(R.id.btninsta);
     }
     
     public void GravarI(View view) {
