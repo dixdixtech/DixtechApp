28acf63b5466091665d860e6dc792d288e0a80c9
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-07 15:29:39 -0300

    Implementando métodos de leitura de dados

--- app/src/main/java/com/example/dixtechapp/Ler.java
+++ app/src/main/java/com/example/dixtechapp/Ler.java
@@ -3,12 +3,72 @@ package com.example.dixtechapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.dixtechapp.Armazenamentos.Type;
 
 public class Ler extends AppCompatActivity {
+    
+    private TextView txtTexto;
+    private Type tipo;
 
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_ler);
+        
+        txtTexto = (TextView) findViewById(R.id.txtleitura);
+        tipo = (Type) getIntent().getSerializableExtra(Armazenamentos.STORAGE_TYPE);
+
+        try {
+
+            if (tipo == Type.INTERNAL) {
+                LerInterno();
+            } else {
+                LerExterno();
+            }
+        }catch (IOException e) {
+            Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_SHORT);
+            e.printStackTrace();
+        }
+    }
+    
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void LerInterno() throws IOException {
+        FileInputStream infl = openFileInput(Armazenamentos.FILE_NAME);
+
+        Scanner scanner = new Scanner(infl);
+        try{
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                sb.append(line).append(System.lineSeparator());
+            }
+            txtTexto.setText(sb.toString());
+        }finally{
+            scanner.close();
+        }
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private void LerExterno() throws IOException{
+        String stats = Environment.getExternalStorageState();
+
+        if (!stats.equals(Environment.MEDIA_MOUNTED)){
+            throw new IOException("O Cartão de memória não foi montado ou encontrado");
+        }
+
+        File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        Scanner scanner = new Scanner( new File(dir, Armazenamentos.FILE_NAME));
+
+        try{
+            StringBuilder sb = new StringBuilder();
+            while (scanner.hasNext()){
+                String line = scanner.nextLine();
+                sb.append(line).append(System.lineSeparator());
+            }
+            txtTexto.setText(sb.toString());
+        }finally{
+            scanner.close();
+        }
     }
 }
