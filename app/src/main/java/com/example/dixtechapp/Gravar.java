c8d0aa3acc8dc620c2943f9ec0195830674ba632
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-07 15:23:04 -0300

    Implementando os métodos de gravação de dados

--- app/src/main/java/com/example/dixtechapp/Gravar.java
+++ app/src/main/java/com/example/dixtechapp/Gravar.java
@@ -3,12 +3,85 @@ package com.example.dixtechapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.example.dixtechapp.Armazenamentos.Type;
 
 public class Gravar extends AppCompatActivity {
+    
+    private static final int EXTERNAL_STORAGE_PERMISSION_CODE = 30;
+    private Type type;
+    private EditText edTexto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gravar);
+        getSupportActionBar().hide();
+
+        edTexto = (EditText) findViewById(R.id.edittextgravacao);
+        type = (Type) getIntent().getSerializableExtra(Armazenamentos.STORAGE_TYPE);
     }
+    
+     @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    public void SalvarTxt(View view) {
+
+        String txt = edTexto.getText().toString();
+        String caminho;
+
+        try {
+            if (type == Type.INTERNAL) {
+                caminho = salvarInterno(txt);
+            } else {
+                caminho = salvarExterno(txt);
+            }
+
+            Toast.makeText(this, "Arquivo gravado em" + caminho,Toast.LENGTH_SHORT).show();
+            finish();
+        } catch (IOException e) {
+            Toast.makeText(this, "Erro: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+            e.printStackTrace();
+        }
+
+    }
+
+    private String gravarInterno(String text) throws IOException {
+
+
+        FileOutputStream out = openFileOutput(Armazenamentos.FILE_NAME,	MODE_PRIVATE);
+        PrintWriter pw = new PrintWriter(out);
+
+        try {
+            pw.print(text);
+            return getFilesDir().getPath() + File.separator
+                    + Armazenamentos.FILE_NAME;
+        } finally {
+            pw.close();
+        }
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+    private String gravarExterno(String text) throws IOException {
+
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                EXTERNAL_STORAGE_PERMISSION_CODE);
+
+        String status = Environment.getExternalStorageState();
+
+        if( !status.equals(Environment.MEDIA_MOUNTED)){
+            throw new IOException("O SD Card não montado ou nãoo disponível");
+        }
+        File dir = getExternalFilesDir(Environment.DIRECTORY_DOCUMENTS);
+        File file = new File(dir, Armazenamentos.FILE_NAME);
+        PrintWriter pw = new PrintWriter(file);
+
+        try{
+            pw.print(text);
+            return file.getPath();
+        }finally {
+            pw.close();
+        }
+
+
+    }
+
 }
