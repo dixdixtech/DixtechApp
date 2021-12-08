50e0277f0654ac3aabcccbd512ad09d12f8e1615
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-07 21:58:20 -0300

    Finalizando a geolocalização

--- app/src/main/java/com/example/dixtechapp/MapsActivity.java
+++ app/src/main/java/com/example/dixtechapp/MapsActivity.java
@@ -12,10 +12,11 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.dixtechapp.databinding.ActivityMapsBinding;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleMap.OnMapLongClickListener, GoogleMap.OnMarkerDragListener {
 
+    private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
-    private ActivityMapsBinding binding;
+   private Geocoder geocoder;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,24 +29,75 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+        geocoder = new Geocoder(this);
     }
 
-    /**
-     * Manipulates the map once available.
-     * This callback is triggered when the map is ready to be used.
-     * This is where we can add markers or lines, add listeners or move the camera. In this case,
-     * we just add a marker near Sydney, Australia.
-     * If Google Play services is not installed on the device, the user will be prompted to install
-     * it inside the SupportMapFragment. This method will only be triggered once the user has
-     * installed Google Play services and returned to the app.
-     */
+   
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+
+        mMap.setOnMapLongClickListener(this);
+        mMap.setOnMarkerDragListener(this);
+
+        try {
+            List<Address> addresses = geocoder.getFromLocationName("R. Guaipá, 678 - Vila Leopoldina, São Paulo - SP, 05089-000", 1);
+            if (addresses.size() > 0) {
+                Address address = addresses.get(0);
+                LatLng leopoldina = new LatLng(address.getLatitude(), address.getLongitude());
+                MarkerOptions markerOptions = new MarkerOptions()
+                        .position(leopoldina)
+                        .title(address.getLocality());
+                mMap.addMarker(markerOptions);
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(leopoldina, 16));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void onMapLongClick(LatLng latLng){
+        try{
+            List<Adrress> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if(addresses.size() > 0){
+                Adress adress = adresses.get(0);
+                String streetAdress = adress.getAdressLine(0);
+                mMap.addMarker(new MarkerOptions()
+                    .position(latLng)
+                    .title(streetAdress)
+                    .draggable(true)          
+                );
+            }
+        } catch (IOExceotion e){
+            e.printStackTrace();
+        }
+    }
+    
+    @Override
+    public void onMarkerDragStart(Marker marker){
+        
+    }
+    
+    @Override
+    public void onMarkerDrag(Marker marker){
+        
+    }
+    
+    @Override
+    public void onMarkerDragEnd(Marker marker){
+        LatLng latLng = marker.getPosition();
+        try{
+            List<Adrress> addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+            if(addresses.size() > 0){
+                Adress adress = adresses.get(0);
+                String streetAdress = adress.getAdressLine(0);
+                marker.setTitle(streetAdress);
+            }
+        } catch (IOExceotion e){
+            e.printStackTrace();
+        }
     }
 }
