5593dbf11554b850bda5510fed55dfbdec4bc0bf
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-08 10:11:34 -0300

    Adicionando floating_button com intent
    Ele leva para a tela de cadastro de serviços.

--- app/src/main/java/com/example/dixtechapp/Servicos.java
+++ app/src/main/java/com/example/dixtechapp/Servicos.java
@@ -6,9 +6,35 @@ import android.os.Bundle;
 
 public class Servicos extends AppCompatActivity {
 
+    RecyclerView recycler;
+    FloatingActionButton btn_add;
+    ImageButton btninfo,btnmap, btnuser, btnhome;
+         
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_servicos);
+        getSupportActionBar().hide();
+        iniciarComponentes():
+        
+        btn_add.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent add = new Intent(Servicos.this, AddServ.class);
+                startActivity(add);
+            }
+        });
+
     }
+    
+    private void iniciarComponentes(){
+        recycler = findViewById(R.id.recyclerServ);
+        btn_add = findViewById(R.id.add_serv);
+        btninfo = findViewById(R.id.btninfoserv);
+        btnmap = findViewById(R.id.btnmapserv);
+        btnuser = findViewById(R.id.btnuserserv);
+        btnhome = = findViewById(R.id.btnhomeserv);
+        }
 }
