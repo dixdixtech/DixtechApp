ba9ee3cc85d8e0e427e973ee3c9dd9225fd287e6
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-07 23:49:00 -0300

    Adicionando action pick
    Ela irá pegar uma imagem da galeria e mostrar na imageview

--- app/src/main/java/com/example/dixtechapp/Usuario.java
+++ app/src/main/java/com/example/dixtechapp/Usuario.java
@@ -12,7 +12,8 @@ import android.widget.TextView;
 public class Usuario extends AppCompatActivity {
 
 ImageButton btnhome, btnmap, btninfo;
-Button btnbio, btnlogout;
+ImageView imgviewfoto;
+Button btnbio, btnlogout, btnfoto;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +24,18 @@ Button btnbio, btnlogout;
 
         IniciarComponentes();
 
+        btnfoto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_DENIED){
+                    String[] permissao = {Manifest.permission.READ_EXTERNAL_STORAGE};
+                    requestPermissions(permissao, 1001);
+                }else{
+                    escolherImagem();
+                }    
+            }
+        });
+            
         btnhome.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -63,7 +76,31 @@ Button btnbio, btnlogout;
 
     }
 
-
+    private void  escolherImagem(){
+        Intent i = new Intent(Intent.ACTION_PICK);
+        I.setType("image/*");
+        startActivityForResult(intent, 1000);
+    }
+    
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data){
+        if(resultCode == RESULT_OK && requestCode == 1000){
+            imgviewfoto.setImageURI(data.getData());
+        }
+    }
+    
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch(request){
+            case 1001:{
+                if(grantResult.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED){
+                    escolherImagem()
+                }else{
+                    Toast.makeText(this, "Permissão negada", Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 
     private void IniciarComponentes() {
         btnhome = findViewById(R.id.btnhomeuser);
@@ -71,6 +108,8 @@ Button btnbio, btnlogout;
         btninfo = findViewById(R.id.btninfouser);
         btnbio = findViewById(R.id.btnbio);
         btnlogout = findViewById(R.id.btnlogoff);
+        btnfoto = findViewById(R.id.btnfotouser);
+        imgviewfoto = findViewById(R.id.imageView);
     }
 
 
