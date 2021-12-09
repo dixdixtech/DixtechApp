c2df953771de4dbb008c99b816314895054dc3a3
Arthur Renan Gutierrez <82537255+arthurrenan00@users.noreply.github.com>
2021-12-08 21:12:57 -0300

    Adicionando o método de cadastro de funcionário(novamente)

--- app/src/main/java/com/example/dixtechapp/FormCadastro.java
+++ app/src/main/java/com/example/dixtechapp/FormCadastro.java
@@ -14,10 +14,10 @@ import android.widget.Toast;
 public class FormCadastro extends AppCompatActivity {
 
     private TextView txttelalogin;
-    private EditText editTextCpf, editTextFone, editTextDataN, editTextSenha, editTextRepeatSenha, editTextNome, editTextCargo, editTextEmail;
+    private EditText editTextCpf, editTextFone, editTextSenha, editTextNome, editTextCargo, editTextEmail;
     private Button botaocad;
-    DBHelper DB;
-
+    DBHelper db= new DBHelper(this);
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -34,32 +34,17 @@ public class FormCadastro extends AppCompatActivity {
                 String cargo= editTextCargo.getText().toString();
                 String senha= editTextSenha.getText().toString();
                 String email= editTextEmail.getText().toString();
-                String dtnasc= editTextDataN.getText().toString();
                 String tel= editTextFone.getText().toString();
 
-                String repeat = editTextRepeatSenha.getText().toString();
-
-                if(TextUtils.isEmpty(cpf) || TextUtils.isEmpty(tel) || TextUtils.isEmpty(dtnasc) || TextUtils.isEmpty(senha) || TextUtils.isEmpty(name)
-                        || TextUtils.isEmpty(cargo) || TextUtils.isEmpty(email))
-                    Toast.makeText(FormCadastro.this, "Todos os campos são requeridos", Toast.LENGTH_SHORT).show();
-                else{
-                    if(senha.equals(repeat)){
-                        Boolean checarcpf = DB.checkcpf(cpf);
-                        if(checarcpf==false){
-                            Boolean insert = DB.insertFunc(name, cpf, cargo, senha, email, dtnasc, tel);
-                            if(insert==true){
-                                Toast.makeText(FormCadastro.this,"Cadastrado com Sucesso!",Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
-                                startActivity(intent);
-                            }else{
-                                Toast.makeText(FormCadastro.this,"Falha ao cadastrar",Toast.LENGTH_SHORT).show();
-                            }
-                        }else{
-                            Toast.makeText(FormCadastro.this,"Sócio já cadastrado",Toast.LENGTH_SHORT).show();
-                        }
-                    }else{
-                        Toast.makeText(FormCadastro.this,"As senhas não combinam entre si",Toast.LENGTH_SHORT).show(); 
-                    }
+                if(name.isEmpty() || cpf.isEmpty() || cargo.isEmpty() || senha.isEmpty() || email.isEmpty() || tel.isEmpty()){ 
+                    //SE ALGUM CAMPO ESTIVER VAZIO, IRÁ MOSTRAR UM TOAST
+                    Toast.makeText(FormCadastro.this, "Os campos são Obrigatoiros",Toast.LENGTH_SHORT).show();
+                }else {
+                    //SE NENHUM CAMPO ESTIVER VAZIO, IRÁ ADICIONAR O FUNCIONÁRIO
+                    db.addFuncionario(new Funcionario(name, cpf, cargo, senha, email, tel));
+                    //TOAST COM A MENSAGEM DE SUCESSO
+                    Toast.makeText(FormCadastro.this, "Funcionario adicionado ", Toast.LENGTH_SHORT).show();
+                    TelaInic();
                 }
             }
         });
@@ -82,14 +67,16 @@ public class FormCadastro extends AppCompatActivity {
 
         editTextCpf = findViewById(R.id.editcpf);
         editTextFone = findViewById(R.id.edittelefone);
-        editTextDataN = findViewById(R.id.editdatanasc);
         editTextSenha = findViewById(R.id.editsenha);
         editTextNome = findViewById(R.id.editnome);
         editTextCargo = findViewById(R.id.editcargo);
         editTextEmail = findViewById(R.id.editemail);
-        editTextRepeatSenha = findViewById(R.id.editrepeatsenha);
         botaocad = findViewById(R.id.btncadastro);
-
-        DB = new DBHelper(this);
+    }
+    
+    public void TelaInic(){
+        Intent inic = new Intent(getApplicationContext(), MainActvity.class);
+        startActivity(inic);
+        finish();
     }
 }
